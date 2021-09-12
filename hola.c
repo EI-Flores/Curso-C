b6d881654b362ac1c89e52cb159356025b9573af
EI-Flores <ing.flores.rivera@gmail.com>
2021-09-12 17:37:48 -0500

    Estructuras y directivas

---
+++ hola.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+    Esto es un comentario en C
+    Esta cosa es una estructura básica en C
+
+    Para usar en Visual Studio Code 
+    https://youtube.com/playlist?list=PLg9145ptuAih4SMmtdWsqIepoBhwKSpF8
+*/
+
+int main(){
+    printf("\n\nHola mundo en C\n");
+    printf("Mi nombre es 'Cosme Fulanito' (Simpsons MX)\n\n");
+    return 0;
+}
